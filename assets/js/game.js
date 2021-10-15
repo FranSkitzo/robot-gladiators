fe2bc4009339114720c8c49ab52434f7025a9792
Rhogar <johnfmartinez597@gmail.com>
2021-10-15 17:31:35 -0400

    fixed blank/null robot name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,19 @@
     return value;
   };
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is" + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   Attack: 10,
   money: 10,
