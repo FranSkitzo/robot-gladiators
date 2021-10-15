bb14b1b68352bd5ecd792ba6fc45f1661f46fc19
Rhogar <johnfmartinez597@gmail.com>
2021-10-14 22:21:05 -0400

    player fights all enemy-robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,9 +10,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert  players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
+  // repeat and  execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+    // place fight function code block here . . .
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     console.log(promptFight);
 
@@ -61,10 +61,17 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
+  }
+
+
+
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  // call fight function with enemy-robot
+  fight(pickedEnemyName);
 }
 
 // Game States
